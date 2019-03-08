1642222ad58c69f6a73a32e567234bfaafe54464
Vergniez Pierre <32926286+PierreV62@users.noreply.github.com>
2019-03-08 16:24:22 +0100

    Maj fin de semaine(fini insc/co)

---
+++ storage/framework/views/86dcb0df7d4723eaf6dce5169bb18345cc3a6986.php
@@ -0,0 +1,51 @@
+<?php $__env->startSection('content'); ?>
+<div class="containerAcceuil"> 
+    <div class="coloneGauche">
+        <div class="videoPresentation">
+        <iframe class="videoPresentation" src="https://www.youtube.com/embed/V_MXGdSBbAI" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
+        <div class="aPropos green">
+            <h2> A propos de nous : </h2>
+            <hr>
+            <p>
+                Bonjour et bienvenue sur La Radinerie, ce site à pour but d'informer les utilisateurs des differentes promos se trouvants à proximité d'eux ou à un endroit precis choisit par celui-ci.
+            </p>
+        </div>
+    </div>
+    
+    <div class="coloneDroite green">
+        <h5> Vous cherchez des promotions près de chez vous ? </h5>
+        <div class="descriptifCarte">
+            <div class="descriptifCarteItem">
+                <label>
+                    Que cherchez vous ? 
+                </label>
+                <input list="type" placeholder="Ex : Restaurant" onkeyup="keyupType(value)">
+                <datalist id="type">
+                </datalist>
+            </div>
+            <div class="descriptifCarteItem">
+                <label>
+                    Quelle type ? 
+                </label>
+                <input list="categorie" placeholder="Ex :Pizzeria " onkeyup="keyupCat(value)">
+                <datalist id="categorie">
+                </datalist>
+            </div>
+            <div class="descriptifCarteItem">
+                <label>
+                    où ? 
+                </label>
+                <input list="ville" placeholder="Ex : Gap" onkeyup="keyupVille(value)">
+                <datalist id="ville">
+                </datalist>
+            </div>
+            
+        </div>
+        <div class=" miniMap" id="map">
+
+        </div>
+    </div>
+</div>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.app', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
